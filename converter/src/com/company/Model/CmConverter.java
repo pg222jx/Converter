4aa13923d9b0ba0d5c3c5515782c7db4e0879883
Filippa Jakobsson <fj222nq@student.lnu.se>
2020-11-18 14:53:03 +0100

    'REFACTOR'

--- converter/src/com/company/Model/CmConverter.java
+++ converter/src/com/company/Model/CmConverter.java
@@ -4,14 +4,8 @@ public class CmConverter {
 
     public double convertToInches(double cm) {
         double inchValue = 0.393701;
-        if (cm == 2) {
-            return inchValue * 2;
-        } else if (cm == 0) {
-            return 0;
-        } else if (cm == -1) {
-            return -0.393701;
-        } else {
-            return inchValue;
-        }
+
+        double inches = cm * inchValue;
+        return inches;
     }
 }
