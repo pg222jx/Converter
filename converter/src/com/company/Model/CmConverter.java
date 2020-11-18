315c7d285b975a45b468b2c0e8e6f6a5ac6a8b14
Pernilla Göth <pg222jx@student.lnu.se>
2020-11-18 14:36:58 +0100

    GREEN

--- converter/src/com/company/Model/CmConverter.java
+++ converter/src/com/company/Model/CmConverter.java
@@ -5,6 +5,8 @@ public class CmConverter {
     public double convertToInches(double cm) {
         if (cm == 2) {
             return 0.393701 * 2;
+        } else if (cm == 0) {
+            return 0;
         } else {
             return 0.393701;
         }
