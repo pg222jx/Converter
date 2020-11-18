c52dbfd7dc31a6dd4e274cac8186527947e21f6b
Pernilla Göth <pg222jx@student.lnu.se>
2020-11-18 14:36:27 +0100

    RED

--- converter/tests/com/company/Model/CmConverterTest.java
+++ converter/tests/com/company/Model/CmConverterTest.java
@@ -33,4 +33,11 @@ public class CmConverterTest {
         double expected = 0.787402;
         assertEquals(expected, actual, 0.000001);
     }
+
+    @Test
+    public void convertToInches_shouldReturnInchesOfTZeroCm() {
+        double actual = sut.convertToInches(0);
+        double expected = 0;
+        assertEquals(expected, actual, 0.000001);
+    }
 }
