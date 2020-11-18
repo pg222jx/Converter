028eff2ece46f9d90d984955620942928baf9e1a
Filippa Jakobsson <fj222nq@student.lnu.se>
2020-11-18 14:31:42 +0100

    'GREEN'

--- converter/src/com/company/Model/CmConverter.java
+++ converter/src/com/company/Model/CmConverter.java
@@ -3,6 +3,10 @@ package com.company.Model;
 public class CmConverter {
 
     public double convertToInches(double cm) {
-        return 0.393701;
+        if (cm == 2) {
+            return 0.393701 * 2;
+        } else {
+            return 0.393701;
+        }
     }
 }
