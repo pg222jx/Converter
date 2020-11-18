ec9c8c82fc98109ee9ba34bf5d40fe9b9c414b99
Pernilla Göth <pg222jx@student.lnu.se>
2020-11-18 14:07:50 +0100

    REFACTOR

--- converter/src/com/company/Model/CmConverter.java
+++ converter/src/com/company/Model/CmConverter.java
@@ -1,8 +1,10 @@
 package com.company.Model;
 
 public class CmConverter {
+    double inchValue = 0.393701;
+
     public double convertToInches(double cm) {
-        double inches = cm * 0.393701;
+        double inches = cm * inchValue;
         return inches;
     }
 }
