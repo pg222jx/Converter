c34237995505f988388847782f235eb1be7e497a
Pernilla Göth <pg222jx@student.lnu.se>
2020-11-20 08:35:27 +0100

    REFACTOR

--- converter/src/com/company/Model/CmConverter.java
+++ converter/src/com/company/Model/CmConverter.java
@@ -2,10 +2,7 @@ package com.company.Model;
 
 public class CmConverter {
 
-    public double convertToInches(double cm) {
-        double inchValue = 0.393701;
-
-        double inches = cm * inchValue;
-        return inches;
+    public double convertFromInches(double cm) {
+        return 0;
     }
 }
--- converter/tests/com/company/Model/CmConverterTest.java
+++ converter/tests/com/company/Model/CmConverterTest.java
@@ -19,32 +19,4 @@ public class CmConverterTest {
     @After
     public void tearDown() throws Exception {
     }
-
-    @Test
-    public void convertToInches_shouldReturnInchesOfOneCm() {
-        double actual = sut.convertToInches(1.0);
-        double expected = 0.393701;
-        assertEquals(expected, actual, 0.000001);
-    }
-
-    @Test
-    public void convertToInches_shouldReturnInchesOfTwoCm() {
-        double actual = sut.convertToInches(2.0);
-        double expected = 0.787402;
-        assertEquals(expected, actual, 0.000001);
-    }
-
-    @Test
-    public void convertToInches_shouldReturnInchesOfTZeroCm() {
-        double actual = sut.convertToInches(0);
-        double expected = 0;
-        assertEquals(expected, actual, 0.000001);
-    }
-
-    @Test
-    public void convertToInches_shouldReturnInchesOfNegativeOneCm() {
-        double actual = sut.convertToInches(-1.0);
-        double expected = -0.393701;
-        assertEquals(expected, actual, 0.000001);
-    }
 }
