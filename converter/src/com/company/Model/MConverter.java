1eee98b773ce87e8fd6406f646b4a794ab6bc4a3
Filippa Jakobsson <fj222nq@student.lnu.se>
2020-11-20 08:59:34 +0100

    'GREEN'

--- converter/src/com/company/Model/MConverter.java
+++ converter/src/com/company/Model/MConverter.java
@@ -3,12 +3,7 @@ package com.company.Model;
 public class MConverter {
 
     public double convertFromInches(double inches) {
-        if (inches == 2) {
-            return 0.0508;
-        } else if (inches == 1){
-            return 0.0254;
-        } else {
-            return -0.0254;
-        }
+        return inches * 0.0254;
+
     }
 }
