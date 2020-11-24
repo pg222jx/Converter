6808e8c0726db11fccba9762fca37189a77d07e5
Pernilla Göth <pg222jx@student.lnu.se>
2020-11-24 11:13:12 +0100

    GREEN

--- converter/src/com/company/Controller/Application.java
+++ converter/src/com/company/Controller/Application.java
@@ -1,17 +1,21 @@
 package com.company.Controller;
 
 import com.company.View.Menu;
+import com.company.View.Input;
 import com.company.Model.MConverter;
 
 public class Application {
-
+    private Menu menu;
+    private MConverter mConverter;
 
     public double start() {
+        Input convertTo = menu.getMenuChoice(menu.getInput());
 
         return 0;
     }
 
     public Application (Menu menu, MConverter mConverter) {
-
+        this.menu = menu;
+        this.mConverter = mConverter;
     }
 }
--- converter/tests/com/company/Controller/ApplicationTest.java
+++ converter/tests/com/company/Controller/ApplicationTest.java
@@ -3,6 +3,8 @@ package com.company.Controller;
 import com.company.View.Input;
 import com.company.View.Menu;
 import com.company.Model.MConverter;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.mockito.Mockito.*;
@@ -15,12 +17,16 @@ public class ApplicationTest {
     private Application spyObject;
 
 
-    @BeforeEach
+    @Before
     public void setUp() {
         menuMock = mock(Menu.class);
         mConverterMock = mock(MConverter.class);
         sut = new Application(menuMock, mConverterMock);
         spyObject = spy(sut);
+    }
+
+    @After
+    public void tearDown() throws Exception {
     }
 
     @Test
