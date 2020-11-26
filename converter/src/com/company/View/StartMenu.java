e8ace235c82869aeb0967f2261128b78b904bb59
Filippa Jakobsson <fj222nq@student.lnu.se>
2020-11-26 20:32:28 +0100

    'GREEN'

--- converter/src/com/company/View/StartMenu.java
+++ converter/src/com/company/View/StartMenu.java
@@ -1,5 +1,7 @@
 package com.company.View;
 
 public class StartMenu {
-    public String getWhatViewMessage() { return ""; }
+    public String getWhatViewMessage() {
+        return "1. English \n2. Svenska\n";
+    }
 }
