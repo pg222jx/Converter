d2d5970f24af7a6554d5f156f998bac4bdfa47e0
Filippa Jakobsson <fj222nq@student.lnu.se>
2020-11-26 18:32:07 +0100

    'REFACTOR'

--- converter/src/com/company/Model/ConverterFactory.java
+++ converter/src/com/company/Model/ConverterFactory.java
@@ -2,8 +2,12 @@ package com.company.Model;
 
 import com.company.View.Input;
 
-
 public class ConverterFactory {
+    /**
+     * @param unit Input
+     * @param value double
+     * @return new MConverter()
+     */
     public static Converter getConverter(Input unit, double value) {
         if (unit.equals(Input.Meter)) return new MConverter(value);
         else {
