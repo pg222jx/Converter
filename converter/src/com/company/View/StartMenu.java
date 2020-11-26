6ec1328181c8f377d7be9aa1003e6c497fef5ba7
Filippa Jakobsson <fj222nq@student.lnu.se>
2020-11-26 20:33:24 +0100

    'REFACTOR'

--- converter/src/com/company/View/StartMenu.java
+++ converter/src/com/company/View/StartMenu.java
@@ -1,5 +1,8 @@
 package com.company.View;
 
+/**
+ * @return - The message with the menu languages.
+ */
 public class StartMenu {
     public String getWhatViewMessage() {
         return "1. English \n2. Svenska\n";
