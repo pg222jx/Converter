82996dec91a771f85f4c3ee00a317c21c8346e44
Filippa Jakobsson <fj222nq@student.lnu.se>
2020-11-26 18:29:54 +0100

    'RED'

--- converter/tests/com/company/Model/ConverterFactoryTest.java
+++ converter/tests/com/company/Model/ConverterFactoryTest.java
@@ -1,15 +1,34 @@
 package com.company.Model;
 
+import com.company.View.Console;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import com.company.View.Input;
 import static org.junit.Assert.assertTrue;
 
 public class ConverterFactoryTest {
+    private ConverterFactory sut;
+
+    @Before
+    public void setUp() throws Exception {
+        sut = new ConverterFactory();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
 
     @Test
     public void getConverter_shouldCreateInstanceOfMConverter() {
         Converter c = ConverterFactory.getConverter(Input.Meter, 1.0);
         assertTrue(c instanceof MConverter);
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getConverter_shouldThrowIllegalArgumentExceptionIfInputFoot() {
+        sut.getConverter(Input.Foot, 1.0);
     }
 
 }
